747fc93bbba76dbf25f2190f5b4bda9f38929e49
zhangliqund <ZHANGLIQUND@enn.cn>
2024-04-23 11:02:20 +0800

    feat:1  完成 第一个简单的serve

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use std::collections::HashMap;
+//se std::collections::HashMap;
+// #[tokio::main]
+// async fn main()->Result<(),Box<dyn std::error::Error>> {
+//     // let resp = reqwest::get("https://www.baidu.com/").await?.json::<HashMap<String,String>>().await?;
+//     let resp = reqwest::get("https://www.rust-lang.org")
+//         .await?
+//         .text()
+//         .await?;
+//     println!("{:#?}",resp);
+//     Ok(())
+// }
+use warp::Filter;
 #[tokio::main]
-async fn main()->Result<(),Box<dyn std::error::Error>> {
-    // let resp = reqwest::get("https://www.baidu.com/").await?.json::<HashMap<String,String>>().await?;
-    let resp = reqwest::get("https://www.rust-lang.org")
-        .await?
-        .text()
-        .await?;
-    println!("{:#?}",resp);
-    Ok(())
+async fn main(){
+    let hello = warp::get().map(|| "hello,世界 !  你说ssss".to_string());
+    warp::serve(hello).run(([127,0,0,1],9090)).await;
 }
